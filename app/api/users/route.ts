8866e241efa22714a25afd7657e8d2c5bedc9f82
Tyler Landtroop <tlandtroop@ufl.edu>
2025-02-20 00:41:54 -0500

    database working

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,16 +1,16 @@
 import { NextResponse } from "next/server";
 
-import prisma from "@/lib/prisma";
+import db from "@/lib/db";
 
 export async function GET() {
-  const users = await prisma.user.findMany();
+  const users = await db.user.findMany();
 
   return NextResponse.json(users);
 }
 
 export async function POST(request: Request) {
   const { email, name } = await request.json();
-  const newUser = await prisma.user.create({
+  const newUser = await db.user.create({
     data: { email, name },
   });
   return NextResponse.json(newUser, { status: 201 });
