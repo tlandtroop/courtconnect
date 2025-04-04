548cd83ebad15342851472ef70f08df10b071622
MaxPlummer <mplummer1@ufl.edu>
2025-04-04 13:39:33 -0400

    Dashboard weather widget

--- components/dashboard/weather.tsx
+++ components/dashboard/weather.tsx
@@ -1,14 +1,15 @@
 import { CloudRain } from "lucide-react";
+import Script from "next/script";
 
 import {
   Card,
   CardContent,
-  CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 
 const Weather = () => {
+  const apiKey = process.env.NEXT_PUBLIC_WEATHER_API;
   return (
     <Card>
       <CardHeader>
@@ -16,32 +17,23 @@ const Weather = () => {
           <CloudRain className="w-5 h-5" />
           Weather
         </CardTitle>
-        <CardDescription>Check conditions before playing</CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="bg-gray-50 p-6 rounded-lg">
-          <div className="flex items-center justify-between mb-4">
-            <div className="text-5xl font-bold">72°</div>
-            <div className="text-right">
-              <div className="font-medium">Partly Cloudy</div>
-              <div className="text-sm text-gray-500">Gainesville, FL</div>
-            </div>
-          </div>
-          <div className="grid grid-cols-3 gap-2 text-center text-sm">
-            <div className="p-2 bg-white rounded">
-              <div className="font-medium">75°</div>
-              <div className="text-gray-500">High</div>
-            </div>
-            <div className="p-2 bg-white rounded">
-              <div className="font-medium">65°</div>
-              <div className="text-gray-500">Low</div>
-            </div>
-            <div className="p-2 bg-white rounded">
-              <div className="font-medium">10%</div>
-              <div className="text-gray-500">Rain</div>
-            </div>
-          </div>
-        </div>
+      <div id="openweathermap-widget-15"></div>
+      <Script 
+        src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js"
+        onReady={() => {
+          window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];  
+          window.myWidgetParam.push({id: 15,cityid: '4156404',appid: apiKey,units: 'imperial',containerid: 'openweathermap-widget-15', });  
+          (function() { 
+            var script = document.createElement('script');script.async = true;
+            script.charset = "utf-8";
+            script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(script, s); 
+          })();
+        }}
+      />
       </CardContent>
     </Card>
   );
